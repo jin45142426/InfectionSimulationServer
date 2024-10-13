52352ee02f101783e41969667e09855c74dfb33d
Kim-Minuk <dduki0729@gmail.com>
2024-10-13 18:29:49 +0900

    update proto

--- Server/Program.cs
+++ Server/Program.cs
@@ -46,12 +46,12 @@ namespace Server
 			TickRoom(room, 50);
 
 			string host = Dns.GetHostName();
-            IPHostEntry ipHost = Dns.GetHostEntry(host);
-            //IPHostEntry ipHost = Dns.GetHostEntry("CGlabHospital.iptime.org");
-            IPAddress ipAddr = ipHost.AddressList[ipHost.AddressList.Length - 1];
-			IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
+			//IPHostEntry ipHost = Dns.GetHostEntry(host);
+			//IPAddress ipAddr = ipHost.AddressList[ipHost.AddressList.Length - 1];
+            IPAddress ipAddr = IPAddress.Parse("0.0.0.0");
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
 
-			_listener.Init(endPoint, () => { return SessionManager.Instance.Generate(); });
+            _listener.Init(endPoint, () => { return SessionManager.Instance.Generate(); });
 			Console.WriteLine("Listening...");
 
 			Profiling(1000);
